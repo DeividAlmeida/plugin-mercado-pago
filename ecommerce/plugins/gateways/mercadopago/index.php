d6795ec5ddf4de28413e4485528ff51864bf79ce
DeividAlmeida <mr.davialmeida@hotmail.com>
2020-10-07 18:06:50 -0300

    integração mercado pago

--- ecommerce/plugins/gateways/mercadopago/index.php
+++ ecommerce/plugins/gateways/mercadopago/index.php
@@ -10,6 +10,7 @@ $mercadopago = DBRead('ecommerce_plugins','*', "WHERE nome = 'mercadopago'")[0];
     <div class="card">
         <div class="card-header white">
         <strong>Configurar Meio de Pagamento</strong>
+        <button class="btn btn-sm behance text-white" data-toggle="modal" data-target="#AjudaMP"><i class="icon-question-circle"></i></button>
     </div>
     <div class="card-body">        
         <div class="row">
@@ -22,6 +23,22 @@ $mercadopago = DBRead('ecommerce_plugins','*', "WHERE nome = 'mercadopago'")[0];
         </div>
     </div>
 
+
+<div class="modal fade MP" id="AjudaMP" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog modal-sm" role="document" >
+			<div class="modal-content" style="border-color: secondary;">
+				<div class="modal-header r-0 bg-primary">
+					<h6 class="modal-title text-white" id="exampleModalLabel">Informações de Sobre o Plug-in</h6>
+					<a href="#" data-dismiss="modal" aria-label="Close" class="paper-nav-toggle paper-nav-white active"><i></i></a>
+				</div>
+
+				<div class="modal-body">
+					<p>
+						Seu servidor deve está rodando com a versão 7.1.0 do PHP ou superior.<br>
+					</p>
+				</div>
+			</div>
+		</div>
 <script>
 function mercadopago(){
     let m = new XMLHttpRequest();
--- ecommerce/plugins/gateways/mercadopago/wa/index.php
+++ ecommerce/plugins/gateways/mercadopago/wa/index.php
@@ -1,31 +1,39 @@
 <?php
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
-$frete = 130.00;
+$reading =  DBRead('ecommerce_mercadopago','*', "WHERE id = '1'")[0];
+
 // Configura credenciais
-MercadoPago\SDK::setAccessToken('APP_USR-6746500504163026-100511-067db8df0d5d351ad6f4efaf67c69c57-177309895');
+MercadoPago\SDK::setAccessToken($reading['token']);
 
 $preference = new MercadoPago\Preference();
 
 $shipments = new MercadoPago\Shipments();
+$frete =   floatval($_POST['vl_frete']);
 $shipments->cost = $frete;
 
   
 # Building an item
 
-$item = new MercadoPago\Item();
-$item->id = "00001";
-$item->title = "item"; 
-$item->quantity = 1;
-$item->unit_price = 100.26;
+    foreach($read as $r){
+        $pdt = json_decode($r['produto'], true);
+        foreach($pdt as $key => $fds){
+        
 
-$item1 = new MercadoPago\Item();
-$item1->id = "00001";
-$item1->title = "item"; 
-$item1->quantity = 1;
-$item1->unit_price = 100.25;
+            $item[$key] = new MercadoPago\Item();
+            $item[$key] ->id = $fds['id_pdt'];
+            $item[$key] ->title = $fds['produto_pg']; 
+            $item[$key] ->quantity = $fds['qtd'];
+            $item[$key] ->unit_price = $fds['un_valor'];
+           
+          
+        }
+    }
+    $itens = array_merge($item);
 
-$preference->items = array($item, $item1);
+  $preference->items  = $itens;
+
+
 $preference->shipments = $shipments;
 $preference->save(); # Save the preference and send the HTTP Request to create
 
