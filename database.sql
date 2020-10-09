369233ddff2c118b089b66380ec473fcf8b21025
DeividAlmeida <mr.davialmeida@hotmail.com>
2020-10-09 18:36:00 -0300

    in set

--- database.sql
+++ database.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `ecommerce_mercadopago` (
   `token` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `ecommerce_mercadopago` (`id`,`token`) VALUES (NULL, NULL);
+INSERT INTO `ecommerce_mercadopago` (`id`,`token`) VALUES (1, NULL);
 
 INSERT INTO `ecommerce_plugins` (`id`, `titulo`, `nome`, `tipo`, `path`, `img`, `status`) VALUES (2, 'Mercado Pago', 'mercadopago', 'gateways', 'ecommerce/plugins/gateways/mercadopago', 'ecommerce/plugins/gateways/mercadopago/wa/assets/img/mercadopago.jpg', '');
